764e58665797c97bd38d266b20d46971d222c03f
r1tsuu <enjoythepain1337@gmail.com>
2024-03-19 23:22:51 +0200

    issue

--- payload.config.ts
+++ payload.config.ts
@@ -23,6 +23,7 @@ import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { buildConfig } from 'payload/config'
 import sharp from 'sharp'
 import { fileURLToPath } from 'url'
+import { postgresAdapter } from '@payloadcms/db-postgres'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -57,19 +58,67 @@ export default buildConfig({
         },
       ],
     },
+    {
+      slug: 'test',
+      fields: [
+        {
+          type: 'tabs',
+          tabs: [
+            {
+              name: 'tabName',
+
+              fields: [
+                {
+                  type: 'array',
+                  name: 'array',
+                  fields: [
+                    {
+                      type: 'text',
+                      name: 'text',
+                      localized: true,
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              name: 'tabnamenocamelcase',
+
+              fields: [
+                {
+                  type: 'array',
+                  name: 'array',
+                  fields: [
+                    {
+                      type: 'text',
+                      name: 'text',
+                      localized: true,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
   ],
+  localization: {
+    locales: ['en', 'ua'],
+    defaultLocale: 'en',
+  },
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  // db: postgresAdapter({
-  //   pool: {
-  //     connectionString: process.env.POSTGRES_URI || ''
-  //   }
-  // }),
-  db: mongooseAdapter({
-    url: process.env.MONGODB_URI || '',
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.POSTGRES_URI || '',
+    },
   }),
+  // db: mongooseAdapter({
+  //   url: process.env.MONGODB_URI || '',
+  // }),
   admin: {
     autoLogin: {
       email: 'dev@payloadcms.com',
